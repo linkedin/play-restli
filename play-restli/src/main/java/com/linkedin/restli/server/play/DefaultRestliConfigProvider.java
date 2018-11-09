0641130e2658facbdad329f00648d52caf315010
Tyler Horth <tylerhorth@outlook.com>
2018-11-09 11:09:36 -0800

    Add Provider abstraction
    Makes it easier for consumers to override providers with their own
    implementations.
    
    Cannot use Provider<T> alone (without casting) due to type erasure.

---
+++ play-restli/src/main/java/com/linkedin/restli/server/play/DefaultRestliConfigProvider.java
@@ -0,0 +1,27 @@
+package com.linkedin.restli.server.play;
+
+import com.linkedin.restli.server.RestLiConfig;
+import com.typesafe.config.Config;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+
+@Singleton
+public class DefaultRestliConfigProvider implements RestliConfigProvider {
+  private RestLiConfig restliConfig;
+
+  @Inject
+  DefaultRestliConfigProvider(Config playConfig) {
+    restliConfig = new RestLiConfig();
+
+    List<String> resourcePackages = playConfig.getStringList("play.restli.resourcePackages");
+
+    restliConfig.addResourcePackageNames(resourcePackages);
+  }
+
+  @Override
+  public RestLiConfig get() {
+    return restliConfig;
+  }
+}
--- play-restli/src/main/java/com/linkedin/restli/server/play/HttpDispatcherProvider.java
+++ play-restli/src/main/java/com/linkedin/restli/server/play/HttpDispatcherProvider.java
@@ -1,36 +1,7 @@
 package com.linkedin.restli.server.play;
 
-import com.linkedin.parseq.Engine;
-import com.linkedin.r2.filter.FilterChain;
-import com.linkedin.r2.filter.transport.FilterChainDispatcher;
 import com.linkedin.r2.transport.http.server.HttpDispatcher;
-import com.linkedin.restli.server.DelegatingTransportDispatcher;
-import com.linkedin.restli.server.RestLiConfig;
-import com.linkedin.restli.server.RestLiServer;
-import com.linkedin.restli.server.resources.ResourceFactory;
-import javax.inject.Inject;
 import javax.inject.Provider;
-import javax.inject.Singleton;
 
 
-@Singleton
-public class HttpDispatcherProvider implements Provider<HttpDispatcher> {
-  private HttpDispatcher httpDispatcher;
-
-  @Inject
-  HttpDispatcherProvider(
-      RestLiConfig restLiConfig,
-      ResourceFactory resourceFactory,
-      Engine engine,
-      FilterChain filterChain
-  ) {
-    RestLiServer restLiServer = new RestLiServer(restLiConfig, resourceFactory, engine);
-    httpDispatcher = new HttpDispatcher(
-        new FilterChainDispatcher(new DelegatingTransportDispatcher(restLiServer, restLiServer), filterChain));
-  }
-
-  @Override
-  public HttpDispatcher get() {
-    return httpDispatcher;
-  }
-}
+public interface HttpDispatcherProvider extends Provider<HttpDispatcher> {}
